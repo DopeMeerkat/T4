7c98ddad244b0410409aa957e60ffad07b7af49f
Dope Meerkat <dopemeerkat@gmail.com>
2020-05-09 15:39:31 -0400

    added basic functions

--- T4/lib/components/grid.dart
+++ T4/lib/components/grid.dart
@@ -2,12 +2,21 @@ import 'move.dart';
 
 class Grid {
   List<List<Move>> grid; //list of rows (first list is y axis, second is x)
+  int turn;
+  List<Move> player;
 
   Grid(row, col) {
     grid = List.generate(row, (i) => new List.filled(col, Move.empty));
+    setTurn(0);
+    player[0] = Move.x; //
+    player[1] = Move.o;
   }
 
   Grid.normal() : this(3, 3);
+
+  void setTurn(int _turn) {
+    turn = _turn;
+  }
 
   int rows() {
     return grid[0].length;
@@ -17,25 +26,53 @@ class Grid {
     return grid.length;
   }
 
-  void extendLeft() {
-    grid.forEach((row) => row.insert(0, Move.empty));
+  // 0 - Left
+  // 1 - Right
+  // 2 - Top
+  // 3 - Bot
+  void extend(int dir) {
+    switch (dir) {
+      case 0:
+        {
+          grid.forEach((row) => row.insert(0, Move.empty));
+        }
+        break;
+      case 1:
+        {
+          grid.forEach((row) => row.add(Move.empty));
+        }
+        break;
+      case 2:
+        {
+          grid.insert(0, List<Move>.filled(cols(), Move.empty));
+        }
+        break;
+      case 3:
+        {
+          grid.add(List<Move>.filled(cols(), Move.empty));
+        }
+        break;
+    }
   }
 
-  void extendRight() {
-    grid.forEach((row) => row.add(Move.empty));
+  bool checkWin() {
+    return false;
   }
 
-  //extend top by adding list to top
-  void extendTop() {
-    grid.insert(0, List<Move>.filled(cols(), Move.empty));
+  bool move(int x, y) {
+    if (grid[x][y] == Move.empty) {
+      grid[x][y] = player[turn % 2];
+      return true;
+    }
+    return false;
   }
 
-  //extend bot by adding list to end
-  void extendBot() {
-    grid.add(List<Move>.filled(cols(), Move.empty));
+  bool block(int x1, y1, x2, y2) {
+    if (grid[x1][y1] == Move.empty && grid[x2][y2] == Move.empty) {
+      grid[x1][y1] = Move.block;
+      grid[x2][y2] = Move.block;
+      return true;
+    }
+    return false;
   }
-  
-  void checkWin() {}
-  void move() {}
-  void block() {}
 }
